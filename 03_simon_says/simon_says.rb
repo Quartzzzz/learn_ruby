b250f52cde12466abd8ef7ce02384c9093cb6132
Quentin Mazet <quentinmazet@laposte.net>
2019-03-02 13:23:55 +0100

    first commit

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,40 @@
-#write your code here
+def echo say
+	say
+end
+
+def shout say
+	say.upcase
+end
+
+def repeat say, i = 2 
+ ((say + ' ') * i).chop
+end
+
+def start_of_word word, i
+	word.byteslice(0, i) 
+end
+
+def first_word sent
+	sent.split(" ")[0] 
+end
+
+def titleize title 
+	words = title.split(" ")
+
+	i = 0 
+
+	while i < words.length
+		word = words[i] 
+		if word == 'and'
+		word[0] = word[0]
+		elsif word == 'over'
+		word[0] = word[0]
+		elsif word == 'the' && word != words[0]
+		word[0] = word[0] 
+		else
+		word[0] = word[0].capitalize
+		end
+		i += 1
+	end
+return words.join(" ") 
+end	
