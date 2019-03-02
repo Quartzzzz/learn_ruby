86d5edd6b0b3579619c9dcc08102f99676f61c63
Quentin Mazet <quentinmazet@laposte.net>
2019-03-02 17:16:14 +0100

    ex05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+	def title=(str)	
+		conj = [ 'for', 'and', 'nor', 'but', 'or', 'yet', 'so' ] 
+		prep = [ 'in' , 'of' ]
+		art = [ 'a' , 'an' , 'the']
+		new_str = str.split(" ").map.with_index do |word, i|
+			if conj.include? word or prep.include? word or art.include? word and i != 0
+				word
+			else word.capitalize
+			end
+		end
+		@title = new_str.join(" ") 
+	end
+	def title
+		@title
+	end
 end
