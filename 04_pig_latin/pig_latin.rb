8157e9fcb0413b3e7341c75447fa3c5450704ba3
Quentin Mazet <quentinmazet@laposte.net>
2019-03-02 13:21:19 +0100

    first commit

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,18 @@
-#write your code here
+def translate word
+	word_letters = word.split("")
+	word_length = word.split(" ").length
+  vowel = [ 'a', 'e', 'i', 'o', 'u', 'y' ]
+  if vowel.include? word[0] and word_length == 1
+  word + 'ay'
+	elsif not vowel.include? word[0] and vowel.include? word[1] and word_length == 1
+	word_letters.push(word_letters.shift).join("") + 'ay'
+	elsif not vowel.include? word[0] and not vowel.include? word[1] and word_length == 1
+	word_letters.push(word_letters.shift(2)).join("") + 'ay'
+	elsif word_length > 1
+	words = word.split(" ").map! do |w|
+	w + 'ay'
+	end
+	words.join(" ") 
+	end	
+end
+
