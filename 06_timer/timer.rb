79cf1262aeb66fd6744e091125c2a6e873b7f2ab
Quentin Mazet <quentinmazet@laposte.net>
2019-03-05 14:05:44 +0100

    Finish

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,14 @@
 class Timer
-  #write your code here
+	def seconds
+		@timer = 00
+	end
+	def seconds=(num)
+		@seconds = num % 60
+		@minutes = num / 60 % 60
+		@hours = num / 3600
+		@timer = num
+	end
+	def time_string
+		@time_string = "%02d:%02d:%02d" % [@hours, @minutes, @seconds] 
+	end
 end
